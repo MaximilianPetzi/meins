705273fbd8073958c5ace26c5bf69ecdf1bc42bc
MaximilianPetzi <maximilianpetzi@gmx.de>
2021-08-19 10:30:53 -0700

    plotting improved

--- plit.py
+++ plit.py
@@ -24,14 +24,24 @@ for i in range(len(am)):
     Am[i,:]=amm[:minl]
 print("data-shape before avg:",np.shape(Am))
 
-yerr=np.std(Am,axis=0)
+#sliding avg:
+siz=15
+slav=np.zeros(np.shape(Am[:,siz-1:]))
+for i in range(len(slav[0])):
+    slav[:,i]=slav[:,i]+np.sum(Am[:,i:i+siz],axis=1)
+slav=slav/siz
+
+yerr=np.std(slav,axis=0)
+print("AAAAAA",np.shape(slav))
+sliding_av=np.average(slav,axis=0)
 Am=np.average(Am,axis=0)
 
-plt.plot(Am)
 
-plt.plot(Am,color=(1,0,0,1),linewidth=.5)
-plt.errorbar(x=range(len(Am)),y=Am, yerr=yerr,color=(1,0,0,.1),elinewidth=.3)
 
+plt.plot(sliding_av,color=(1,0,0,1),label="sliding avg")
+plt.plot(Am,color="black",linewidth=.4)
+plt.errorbar(x=range(len(sliding_av)),y=sliding_av, yerr=yerr,color=(1,0,0,.2),elinewidth=.5,label="error of sliding avg")
+plt.legend()
 plt.savefig("../bilder/plit_temp/this")
 plt.show()
 
